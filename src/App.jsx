6d552aa4f6e76bfc87744a69922fef8d8acfbacc
Abhinandh <abhinandh20011@gmail.com>
2025-03-12 12:38:05 +0530

    Added more Rows

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import Banner from './Components/banner/banner'
 import Row from './Components/cards/Row'
-import {actions,originals} from './Url'
+import {actions,originals,trending,ComedyMovies,HorrorMovies,RomanceMovies} from './Url'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,9 +13,14 @@ function App() {
     <>
      <Navbar />
      <Banner/>
-     <Row url={originals} title='Netflix Originals' /> 
-     <Row url={actions} title='Actions' isSmall /> 
-     <Row url={actions} title='Actions' isSmall /> 
+     <Row url={originals} title='Netflix Originals'/> 
+     <Row url={trending} title='Trending Movies'/> 
+    <Row url={ComedyMovies} title='Comedy Movies' isSmall /> 
+     <Row url={actions} title='Actions Movies'/> 
+     <Row url={HorrorMovies} title='Horror Movies' isSmall /> 
+     <Row url={RomanceMovies} title='Romance Movies'/> 
+     
+     
     </>
   )
 }
